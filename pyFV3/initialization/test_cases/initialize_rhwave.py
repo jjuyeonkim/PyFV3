ddd517a22f2c27041d7ad670fbd8940a4f4d13ae
Janice Kim <janice.kim@noaa.gov>
2025-04-11 09:31:12 -0400

    [WIP] Another stopping point.

--- pyFV3/initialization/test_cases/initialize_rhwave.py
+++ pyFV3/initialization/test_cases/initialize_rhwave.py
@@ -10,9 +10,94 @@ from ndsl.grid.gnomonic import great_circle_distance_lon_lat, lon_lat_midpoint
 from pyFV3.dycore_state import DycoreState
 from pyFV3.initialization import init_utils
 
-def _rhwave_initialization():
+def preinit_for_all_sw(state: DycoreState,
+                       shape,
+                       grid_data: GridData
+):
     """
-    TODO What does this do?
+    Pre-initialization from test_cases.F90 that applies to all shallow water tests
+
+    Args:
+        state: DycoreState - modified
+        shape: TODO
+    """
+    
+    nx, ny, nz = init_utils.local_compute_size(shape)
+    
+    # Initializing to values the Fortran does for easy comparison
+    state.pe[:] = 0.0
+    state.pt[:] = 1.0
+
+    '''
+    # TODO: How is this the same as below?
+    # NOTE: 'is' is like 'isc' == i start compute domain (no halos)
+      delp(isd:is-1,jsd:js-1,1:npz)=0.
+      delp(isd:is-1,je+1:jed,1:npz)=0.
+      delp(ie+1:ied,jsd:js-1,1:npz)=0.
+      delp(ie+1:ied,je+1:jed,1:npz)=0.
+    '''
+    NHALO = constants.N_HALO_DEFAULT # TODO: Where to put this?
+    SURFACE_PRESSURE = 1.0e5  # units of (Pa), from Table VI of DCMIP2016 #TODO: where to put this?
+
+    # Basically the corners of the 
+    state.delp[:NHALO, :NHALO] = 0.0
+    state.delp[:NHALO, NHALO + ny :] = 0.0
+    state.delp[NHALO + nx :, :NHALO] = 0.0
+    state.delp[NHALO + nx :, NHALO + ny :] = 0.0
+
+    '''
+      f0(:,:) = huge(dummy)
+      fC(:,:) = huge(dummy)
+      do j=jsd,jed+1
+         do i=isd,ied+1
+            fC(i,j) = 2.*omega*( -1.*cos(grid(i,j,1))*cos(grid(i,j,2))*sin(alpha) + &
+                                     sin(grid(i,j,2))*cos(alpha) )
+         enddo
+      enddo
+      do j=jsd,jed
+         do i=isd,ied
+            f0(i,j) = 2.*omega*( -1.*cos(agrid(i,j,1))*cos(agrid(i,j,2))*sin(alpha) + &
+                                     sin(agrid(i,j,2))*cos(alpha) )
+         enddo
+      enddo
+      call mpp_update_domains( f0, domain )
+      if (cubed_sphere) call fill_corners(f0, npx, npy, YDir)#end
+    '''
+    #fC = grid_data.fC() # TODO: already in gridData?, but I don't know if they match?
+    
+    # TODO: Below, taken from baroclinic test. Do we need these?
+    # TODO: Should these be in postinit?
+    state.ua[:] = 1e35
+    state.va[:] = 1e35
+    state.uc[:] = 1e30
+    state.vc[:] = 1e30
+    state.w[:] = 1.0e30
+    state.delz[:] = 1.0e25
+    state.phis[:] = 1.0e25
+    state.ps[:] = SURFACE_PRESSURE
+    eta = np.zeros(nz)
+    eta_v = np.zeros(nz)
+    islice, jslice, slice_3d, slice_2d = init_utils.compute_slices(nx, ny)
+    init_utils.setup_pressure_fields(
+        eta=eta,
+        eta_v=eta_v,
+        delp=state.delp[slice_3d],
+        ps=state.ps[slice_2d],
+        pe=state.pe[slice_3d],
+        peln=state.peln[slice_3d],
+        pk=state.pk[slice_3d],
+        pkz=state.pkz[slice_3d],
+        ak=utils.asarray(grid_data.ak.data),
+        bk=utils.asarray(grid_data.bk.data),
+        ptop=grid_data.ptop,
+    )
+
+    
+def init_for_rhwave(state: DycoreState,
+                    grid_data: GridData
+):
+    """
+    Initialization specific to Rossby Wave number 4 from test_cases.F90
 
     TODO Update Inputs eventually; for now, taken from baroclinic initialization
 
@@ -27,7 +112,7 @@ def _rhwave_initialization():
     r = 4.0 # TODO What is r?
     omg = 7.848e-6
     rk    = 7.848e-6
-    phis = 0.0 # TODO: Why is this phis scalar, while the DycoreState version is 2d?
+    phis = 0.0 # TODO: Why is this phis scalar but used as 2d array below?
     '''
     # JKNOTE: This looks like it's iterating over a 2D grid to determine the A, B, C values?
     # JKNOTE: delp is pressure_thickness_atmospheric_layer
@@ -49,29 +134,24 @@ def _rhwave_initialization():
             enddo
          enddo
     '''
-    """ From test_cases.F90:
-
-      case(6)
-         #Ubar = 0.
-         gh0  = 8.E3*Grav
-         R    = 4.
-         omg  = 7.848E-6
-         rk    = 7.848E-6
-         phis = 0.0
-         do j=js,je
-            do i=is,ie
-               A = 0.5*omg*(2.*omega+omg)*(COS(agrid(i,j,2))**2) + &
-                   0.25*rk*rk*(COS(agrid(i,j,2))**(r+r)) * &
-                   ( (r+1)*(COS(agrid(i,j,2))**2) + (2.*r*r-r-2.) - &
-                     2.*(r*r)*COS(agrid(i,j,2))**(-2.) )
-               B = (2.*(omega+omg)*rk / ((r+1)*(r+2))) * (COS(agrid(i,j,2))**r) * &
-                    ( (r*r+2.*r+2.) - ((r+1.)*COS(agrid(i,j,2)))**2 )
-               C = 0.25*rk*rk*(COS(agrid(i,j,2))**(2.*r)) * ( &
-                   (r+1) * (COS(agrid(i,j,2))**2.) - (r+2.) )
-               delp(i,j,1) =gh0 + radius*radius*(A+B*COS(r*agrid(i,j,1))+C*COS(2.*r*agrid(i,j,1)))
-               delp(i,j,1) = delp(i,j,1) - phis(i,j)
-            enddo
-         enddo
+    # TODO: What is agrid(i, j, 2), agrid(i, j, 2)
+    # lon_agrid=utils.asarray(grid_data.lon_agrid.data[slice_2d_buffer]),
+    # lat_agrid=utils.asarray(grid_data.lat_agrid.data[slice_2d_buffer]),
+    agd1 = grid_data.lat_agrid.data[:] # TODO: likely wrong
+    agd2 = grid_data.lon_agrid.data[:] # TODO: likely wrong
+    
+    A = (0.5 * omg * (2 * constants.OMEGA + omg) * (np.cos(agd2)**2)
+         + 0.25 * rk * rk * (np.cos(agd2)**(r + r))
+         * ((r + 1) * (np.cos(agd2)**2) + (2 * r * r - r - 2) - 2 * (r * r) * np.cos(agd2)**(-2)))
+    B = ((2 * (constants.OMEGA + omg) * rk / ((r+1) * (r+2)))
+         * (np.cos(agd2)**r) * ((r*r+2 * r + 2) - ((r + 1) * np.cos(agd2))**2 ))
+    C = 0.25 * rk * rk * (np.cos(agd2)**(2 * r)) * ((r + 1) * (np.cos(agd2)**2) - (r+2))
+    
+    state.delp[:,:,1] = (gh0 + constants.RADIUS * constants.RADIUS
+                         * ( A + B * np.cos(r * agd1) + C * np.cos(2 * r * agd1)))
+    state.delp[:,:,1] = state.delp[:,:,1] - phis # TODO: Subtract 0?
+    
+    """ From test_cases.F90 (case 6): 
          do j=js,je
             do i=is,ie+1
                p1(:) = grid(i  ,j ,1:2)
@@ -104,9 +184,58 @@ def _rhwave_initialization():
          call atoc(ua,va,uc,vc,dx,dy,dxa,dya,npx,npy,ng, gridstruct%bounded_domain, domain, bd)
          initWindsCase=initWindsCase6
     """
-    pass
+
+    
+def postinit_for_all_sw(state):
+    """ Post-initialization from test_cases.F90 that applies to all shallow water tests
+
+    Args:
+        state: DycoreState - modified
+    """
+    
+    ''' TODO
+      cl = get_tracer_index(MODEL_ATMOS, 'cl')
+      cl2 = get_tracer_index(MODEL_ATMOS, 'cl2')
+      if (cl > 0 .and. cl2 > 0) then
+         call terminator_tracers(is,ie,js,je,isd,ied,jsd,jed,npz, &
+              q, delp,ncnst,agrid(isd:ied,jsd:jed,1),agrid(isd:ied,jsd:jed,2),bd)
+         call mpp_update_domains(q,domain)
+      endif
+    '''
+
+    ''' TODO: Is this right?
+      do z=2,npz
+         delp(:,:,z) = delp(:,:,1)
+      enddo
+    '''
+    state.delp[:,:,1:] = state.delp[:,:,0][:,:,np.newaxis] # TODO: Is this right?
+    
+    ''' TODO: Can I ignore these mpp_update_domains calls?
+      call mpp_update_domains( delp, domain )
+      call mpp_update_domains( phis, domain )
+    '''
+    
+    ''' TODO:
+      phi0  = delp
+
+      call init_winds(UBar, u,v,ua,va,uc,vc, initWindsCase, npx, npy, ng, ndims, nregions, gridstruct%bounded_domain, gridstruct, domain, tile, bd)
+! Copy 3D data for Shallow Water Tests
+      do z=2,npz
+         u(:,:,z) = u(:,:,1)
+         v(:,:,z) = v(:,:,1)
+      enddo
+
+      do j=js,je
+         do i=is,ie
+            ps(i,j) = delp(i,j,1)
+         enddo
+      enddo
 
 
+call mp_update_dwinds(u, v, npx, npy, npz, domain, bd)
+    '''    
+
+    
 def init_rhwave_state(
     grid_data: GridData,
     quantity_factory: QuantityFactory,
@@ -130,22 +259,11 @@ def init_rhwave_state(
     # TODO: What exactly is this? Found in both tc and baroclinic inits.
     sample_quantity = grid_data.lat
     shape = (*sample_quantity.data.shape[0:2], grid_data.ak.data.shape[0])
-    nx, ny, nz = init_utils.local_compute_size(shape)
     numpy_state = init_utils.empty_numpy_dycore_state(shape)
 
-    # Initializing to values the Fortran does for easy comparison
-    numpy_state.pe[:] = 0.0
-    numpy_state.pt[:] = 1.0
-    
-    NHALO = constants.N_HALO_DEFAULT # TODO: Where to put this?
-    numpy_state.delp[:NHALO, :NHALO] = 0.0
-    numpy_state.delp[:NHALO, NHALO + ny :] = 0.0
-    numpy_state.delp[NHALO + nx :, :NHALO] = 0.0
-    numpy_state.delp[NHALO + nx :, NHALO + ny :] = 0.0    
-    # TODO: more numpy_state.* initialization?
-    
-    _rhwave_initialization(
-    )
+    preinit_for_all_sw(numpy_state, shape, grid_data)
+    init_for_rhwave(numpy_state, grid_data)
+    postinit_for_all_sw(numpy_state)
 
     # TODO: Actual DycoreState init
     state = DycoreState.init_from_numpy_arrays(
