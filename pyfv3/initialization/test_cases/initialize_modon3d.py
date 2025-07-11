1f86def4fe07d8ae4a48052732ba914292e0d9d5
Janice Kim <janice.kim@noaa.gov>
2025-07-11 17:45:31 -0400

    [WIP] Start of westerly winds...

--- pyfv3/initialization/test_cases/initialize_modon3d.py
+++ pyfv3/initialization/test_cases/initialize_modon3d.py
@@ -83,8 +83,99 @@ def _init_modon_pressure_fields(grid_data: GridData, numpy_state: SimpleNamespac
     pk_slice_3d[:, :, 1:] = np.exp(constants.KAPPA * peln_slice_3d[:, :, 1:])
 
 
-def _init_westerly_wind_burst():
-   pass
+def _init_westerly_wind_burst(grid_data: GridData, numpy_state: SimpleNamespace, shape: tuple):
+    """
+    TODO desc
+    Args:
+        grid_data: GridData
+        numpy_state: DycoreState modified to initiate u, v westerly winds
+        shape: tuple
+    """
+    soliton_umax = Float(50.0)
+    soliton_size = Float(750.0e3)
+
+    ubar = soliton_umax
+    r0 = soliton_size
+
+    p0w = (Float(constants.PI*0.5), Float(0.))
+    p0e = (p0w[0] + constants.PI, Float(0.))
+
+    # Calculate helper slices for delp, u+v winds
+    # similar to init_utils.compute_slices(nx, ny)
+    nx, ny, nz = init_utils.local_compute_size(shape)
+    islice = slice(NHALO, NHALO + nx)
+    islice_xtra = slice(NHALO, NHALO + nx + 1)
+    jslice = slice(NHALO, NHALO + ny)
+    jslice_xtra = slice(NHALO, NHALO + ny + 1)
+
+   # TODO: Add the p1, p2 grid pairs to the init_utils?
+    grid = np.transpose(
+        np.stack(  # TODO: Refactor to non-protected _horizontal_data
+            [grid_data._horizontal_data.lon.data, grid_data._horizontal_data.lat.data]
+        ),
+        [1, 2, 0],
+    )
+
+    p1 = grid[:-1, :, :]
+    p2 = grid[1:, :, :]
+    u_2d_buffer = (islice, jslice_xtra)
+    u_buffer_0 = (islice, jslice_xtra, 0)
+    #numpy_state.u[u_buffer_0] = _calc_modon_wind_burst(p1, p2)[u_2d_buffer]
+
+    muv = init_utils._find_midpoint_unit_vectors(
+        p1, p2
+    )  # TODO: Refactor to non-protected call
+    p3 = muv["midpoint"]
+    e2 = muv["unit_dir"]
+    ex = muv["exv"]
+    ey = muv["eyv"]
+
+    #r = great_circle_distance_lon_lat?
+    r = 1 # TODO: Get the actual great circle distance? which one?
+    #r = great_circle_dist( p0w, p3, radius )
+
+    utmp = ubar*np.exp(-(r/r0)**2)
+    #call get_unit_vect2(p1, p2, e2)
+    #call get_latlon_vector(p3, ex, ey)
+    # TODO: iterate over k --- numpy_state.v[:, :, k] = utmp*np.sum(e2*ex, 2) # TODO: set a slice instead of the entire thing? # TODO: double-check innerprod?
+
+    """
+! Initiate the westerly-wind-burst:
+         ubar = soliton_Umax
+         r0 = soliton_size
+         p0w(1) = pi*0.5
+         p0w(2) = 0.
+         p0e(1) = p0w(1) + pi
+         p0e(2) = 0.
+
+
+     do k=1,npz
+        do j=js,je
+           do i=is,ie+1
+              p1(:) = grid(i  ,j ,1:2)
+              p2(:) = grid(i,j+1 ,1:2)
+              call mid_pt_sphere(p1, p2, p3)
+              r = great_circle_dist( p0w, p3, radius )
+              utmp = ubar*exp(-(r/r0)**2)
+              call get_unit_vect2(p1, p2, e2)
+              call get_latlon_vector(p3, ex, ey)
+              v(i,j,k) = utmp*inner_prod(e2,ex)
+           enddo
+        enddo
+        do j=js,je+1
+           do i=is,ie
+              p1(:) = grid(i,  j,1:2)
+              p2(:) = grid(i+1,j,1:2)
+              call mid_pt_sphere(p1, p2, p3)
+              r = great_circle_dist( p0w, p3, radius )
+              utmp = ubar*exp(-(r/r0)**2)
+              call get_unit_vect2(p1, p2, e1)
+              call get_latlon_vector(p3, ex, ey)
+              u(i,j,k) = utmp*inner_prod(e1,ex)
+           enddo
+        enddo
+    """
+    pass
 
 
 def _init_easterly_wind_burst():
@@ -110,7 +201,7 @@ def init_state(
 
     _init_background_state(numpy_state)
     _init_modon_pressure_fields(grid_data, numpy_state, shape)
-    _init_westerly_wind_burst()
+    _init_westerly_wind_burst(grid_data, numpy_state, shape)
     _init_easterly_wind_burst()
     _convert_back_to_temperature()
     # Nest Test?
